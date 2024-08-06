7ba01b934cf94ccc334ff4c3951ed2b2a7598067
NeoJarma <neojarmawijaya@gmail.com>
2024-08-06 10:22:45 +0700

    fix: triangle drawer

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	drawEquilateralTriangle(10)
+	drawEquilateralTriangle(5)
 }
 
 func drawEquilateralTriangle(stack int) {
@@ -13,15 +13,15 @@ func drawEquilateralTriangle(stack int) {
 	}
 
 	// outer loop, equal to the max line of the input
-	for i := 0; i < stack; i++ {
+	for i := 0; i <= stack; i++ {
 		// looping to skip printing character "1"
-		for j := 0; j < stack-i-1; j++ {
+		for j := stack; j > i; j-- {
 			fmt.Print(" ")
 		}
 
 		// looping for print character "1"
-		for k := 0; k < (i*2)+1; k++ {
-			fmt.Print("1")
+		for k := 0; k < i; k++ {
+			fmt.Print("1 ")
 		}
 
 		// start a new line for the next loop
